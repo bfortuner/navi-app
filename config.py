e8dc3062a946c1d101e3514ba3a6d08c2f6c0833
bfortuner <bfortuner@gmail.com>
2013-11-09 10:56:05 -0800

    updated application.py for navi

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SECRET_KEY = '298738soinsoba09u2623insk982'
 DB_USER = 'bfortuner'
 DB_PASSWD = 'AObort90'
 DB_HOST = 'redhatdb2.c66nl1qvonl2.us-west-2.rds.amazonaws.com'
-DB_NAME = 'problemlib'
+DB_NAME = 'navi'
 
 
 # Problem categories                                                                                                                                                                                  
